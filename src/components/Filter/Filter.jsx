04a0b4ef7d544ab7a9aac109f157d8faf652947a
PawelPalasinski <pawel.palasinski@gmail.com>
2022-09-25 00:49:26 +0200

    test

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,6 @@
 import styles from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux';
-import * as actions from '../../redux/actions';
-
 const Filter = ({ filter, onFilter }) => {
   return (
     <input className={styles.filter}
@@ -21,14 +18,4 @@ Filter.propTypes = {
   onChange: PropTypes.func,
 };
 
-// export default Filter;
-
-const mapStateToProps = state => ({
-  value: state.contacts.filter,
-});
-
-const mapDispatchToProps = dispatch => ({
-  onChange: event => dispatch(actions.changeFilter(event.target.value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+export default Filter;
